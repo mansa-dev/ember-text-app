5af9a1a239b8e21cfb71bb6b51b1d6070e3568da
Sushant <sushant@mansainfotech.com>
2015-08-19 12:38:19 +0530

    Added API routes

---
+++ routes/api/text.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+//get all the texts
+router.get('/', function(req, res){
+  res.json();
+});
+
+router.post('/', function(req, res){
+
+});
+
+module.exports = router;
